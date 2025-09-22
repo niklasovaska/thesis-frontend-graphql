d82186a301a44fe3c63b18cad271353aa9dc9608
niklasovaska <niklas.ovaska@gmail.com>
2025-09-22 16:40:33 +0300

    unused queries removed

--- src/graphql/albums/queries.ts
+++ src/graphql/albums/queries.ts
@@ -1,23 +1,6 @@
 import { gql } from '@apollo/client'
 import { REVIEW_STATS_FRAGMENT } from '../fragments/fragment.queries'
 
-export const GET_ALBUMS = gql`
-  query {
-    getAllAlbums {
-      id
-      title
-      slug
-      year
-      artist {
-        name
-      }
-      genre {
-        name
-      }
-    }
-  }
-`
-
 export const GET_ALBUMS_PAGINATED = gql`
   query GetAlbumsPaginated($cursor: String, $limit: Int!){
     getAlbumsPaginated(cursor: $cursor, limit: $limit) {
@@ -39,27 +22,6 @@ export const GET_ALBUMS_PAGINATED = gql`
   }
 
 `
-
-export const GET_ALBUM_BY_ID = gql`
-  query GetAlbumById($id: String!) {
-    getAlbumById(id: $id) {
-      title
-      year
-      imageUrl
-      artist {
-        name
-      }
-      genre {
-        name
-      }
-      tracks {
-        trackNumber
-        title
-      }
-    }
-  }
-`
-
 export const GET_ALBUM_BY_SLUG = gql`
   query GetAlbumsBySlug($slug: String!) {
     getAlbumBySlug(slug: $slug) {

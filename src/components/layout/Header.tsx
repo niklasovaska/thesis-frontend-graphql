8df1320247491465510b50cfb6a61700247af760
niklasovaska <niklas.ovaska@gmail.com>
2025-09-22 16:34:01 +0300

    route links to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,15 @@
+import { Link } from "react-router"
 import ThemeToggle from "../features/ThemeToggle"
 
 const Header = () => {
     return(
         <div className='flex items-center justify-between px-10 bg-muted border-black/50 border-b h-[80px]'>
-            <div className='text-2xl font-bold'>Album collection</div>
+            <Link to={'/'}><div className='text-2xl font-bold'>Album collection</div></Link>
             <ul className='flex items-center gap-6 font-bold'>
-                <li>
-                    Home
-                </li>
-                <li>
-                    Sign in
-                </li>
+                <Link to={'/'}>
+                    <li>Home</li>
+                </Link>
+                <li>Sign in</li>
                 <ThemeToggle />
             </ul>
         </div>
